7a63446df33927a7caba339ba277ac234d42705c
Andrew Bradley <cspotcode@gmail.com>
2017-11-26 17:35:25 -0500

    Switch to 20x faster implementation of ConvertTo-BufferCellArray - For some reason, instantiating and filling the array manually is much faster than the native NewBufferCellArray method

--- powertab/ConsoleLib.ps1
+++ powertab/ConsoleLib.ps1
@@ -412,9 +412,42 @@ Function ConvertTo-BufferCellArray {
         ,
         [System.ConsoleColor]
         $BackgroundColor = $UI.BackgroundColor
+        ,
+        [int]
+        $Width = 0
     )
 
-    ,$UI.NewBufferCellArray($Content, $ForegroundColor, $BackgroundColor)
+    # If width is unspecified, auto-detect max string length
+    if($Width -eq 0) {
+        ForEach($Row in $Content) {
+            $l = $Row.Length
+            if($l -gt $Width) {
+                $Width = $l
+            }
+        }
+    }
+    $Height = $Content.Count
+    $arr = [System.Management.Automation.Host.BufferCell[,]]::new($height, $width)
+    $cell = $arr[0,0]
+    # All cells have the same colors
+    $cell.ForegroundColor = $ForegroundColor
+    $cell.BackgroundColor = $BackgroundColor
+    for($y = 0; $y -lt $height; $y++) {
+        $row = $Content[$y]
+        $rowLength = $row.Length
+        # Fill row with available characters
+        for($x = 0; $x -lt $rowLength; $x++) {
+            $cell.Character = $row[$x]
+            $arr[$y, $x] = $cell
+        }
+        # If row is too short, fill remaining cells with spaces
+        $cell.Character = ' '
+        for(; $x -lt $Width; $x++) {
+            $arr[$y, $x] = $cell
+        }
+    }
+    # Prevent pipe from iterating array
+    ,$arr
 }
 
 Function Parse-List {
